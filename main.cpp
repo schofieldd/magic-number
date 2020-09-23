353858a8dba2369724caa5e330416fe6e9a8e11f
Carlos R. Arias <arias@spu.edu>
2020-09-23 10:25:00 -0700

    Phase 2 Done
    Now the player can play many times.

--- main.cpp
+++ main.cpp
@@ -10,30 +10,38 @@ using std::cerr;
 int main() {
 	srand(time(0));
 
-	int numberToGuess = rand() % 10 + 1;
-	// cout << "- - - - " << numberToGuess << " - - - -" << endl << endl;
+	int numberToGuess;
 	int userGuess;
 	bool won = false;
+	int keepGoing = 0;
 
-	for (int i = 0; i < 3; ++i) {
-		cout << "Give me your guess #" << i + 1 << ": ";
-		cin >> userGuess;
-		if (userGuess == numberToGuess){
-			won = true;
-			break;
-		}else{
-			cout << "Guess Again..." << endl;
+	do{
+		numberToGuess = rand() % 10 + 1;
+		// cout << "- - - - " << numberToGuess << " - - - -" << endl << endl;
+		for (int i = 0; i < 3; ++i) {
+			cout << "Give me your guess #" << i + 1 << ": ";
+			cin >> userGuess;
+			if (userGuess == numberToGuess){
+				won = true;
+				break;
+			}else{
+				cout << "Guess Again..." << endl;
+			}
 		}
-	}
-	/**
-	 * if (won == true) {} --> How you should write
-	 * if (won) {} --> you need to be able to read it
-	 */
-	if (won){
-		cout << "You are a Winner!!!" << endl;
-	}else{
-		cout << "Loser :(" << endl;
-	}
+		/**
+		 * if (won == true) {} --> How you should write
+		 * if (won) {} --> you need to be able to read it
+		 */
+		if (won){
+			cout << "You are a Winner!!!" << endl;
+		}else{
+			cout << "Loser :(" << endl;
+		}
+		cout << "Would you like to play another game? [1 for yes]: ";
+		cin >> keepGoing;
+	}while (keepGoing == 1);
+
+	cout << "Thanks for playing my game" << endl;
 
 	return 0;
 }
