0153f1f9897b13ca7ebe064ef86a33e95b93194c
schofieldd <schofieldd@spu.edu>
2020-09-24 13:14:48 -0700

    hot, warm, cold etc
    Have the code assigning hot, warm, cold or freezing based on difference between user guess and actual answer

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 
 using std::cout;
 using std::cin;
@@ -21,10 +22,23 @@ int main() {
 	int maximumGuesses, maximumToGuess;
 	bool won = false;
 	int keepGoing = 0;
+	int hot, warm, cold;
+	double logVal;
 
 	do{
 		cout << "What is the maximum number to guess: ";
 		cin >> maximumToGuess;
+		//Now determine the values to assign hot...cold
+		//Going to do this with logs which might be weird
+		logVal = log10(static_cast<double>(maximumToGuess));
+		hot = maximumToGuess / (static_cast<int>(logVal * 10));
+		warm = hot * 3;
+		cold = hot * 5;
+		cout << "---" << "Hot is within: " << hot << endl;
+        cout << "---" << "warm is within: " << warm << endl;
+        cout << "---" << "cold is within: " << cold << endl;
+
+
 		cout << "How many opportunities you want: ";
 		cin >> maximumGuesses;
 
@@ -37,6 +51,19 @@ int main() {
 				won = true;
 				break;
 			}else{
+			    if (abs(numberToGuess - userGuess) < hot){
+                    cout << "Oooo you're hot" << endl;
+                }
+                else if (abs(numberToGuess - userGuess) < warm) {
+                    cout << "You're warm-ish" << endl;
+                }
+                else if (abs(numberToGuess - userGuess) < cold) {
+                    cout << "You're cold" << endl;
+                }
+                else {
+                    cout << "Brrrr you're freezing" << endl;
+                }// Have to give a bit more of a hint here - DPS
+
 				cout << "Guess Again..." << endl;
 			}
 		}
